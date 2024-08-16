af76c9e1edcce1ff2fc9ffbd74b5a369d8c76d56
Pratibha <pc634805@gmail.com>
2024-08-16 16:28:44 +0530

    added the controllers of the previous project GJSchool

---
+++ src/main/java/com/academics/controllers/AdminController.java
@@ -0,0 +1,92 @@
+package com.academics.controllers;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+
+@RestController
+public class AdminController {
+	@Autowired
+	//public AdminDao adminDao;
+	
+	
+	
+	
+	@RequestMapping("/classes")
+	public String classes() {
+		
+		return "addClasses";
+	}
+	
+	@RequestMapping("/addClasses")
+	public String addClasses() {
+	
+		return "redirect:/classes";
+	}
+	
+	
+	@RequestMapping("/session")
+	public String session() {
+		
+		return "addSession";
+	}
+	
+	@RequestMapping("/addsession")
+	public String addSession() {
+	
+		return "redirect:/session";
+	}
+	
+
+	@RequestMapping("/category")
+	public String category() {
+		
+		return "addCategory";
+	}
+	
+	@RequestMapping("/addcategory")
+	public String addCategory() {
+	
+		return "redirect:/category";
+	}
+	
+
+	@RequestMapping("/feestype")
+	public String feestype() {
+		
+		return "addFeesType";
+	}
+	
+	@RequestMapping("/addfeestype")
+	public String addfeestype() {
+
+		return "redirect:/feestype";
+	}
+	
+	@RequestMapping("/deletefeestype")
+	public String delteFeesType() {
+		
+		return "redirect:/feestype";
+	}
+	
+	@RequestMapping("/deleteclasses")
+	public String delteClasses() {
+		
+		return "redirect:/classes";
+	}
+	
+	
+	@RequestMapping("/deletesession")
+	public String delteSession() {
+		
+		return "redirect:/session";
+	}
+	@RequestMapping("/deletecategory")
+	public String delteCategory() {
+		
+		return "redirect:/category";
+	}
+	
+
+}
--- src/main/java/com/academics/controllers/StudentController.java
+++ src/main/java/com/academics/controllers/StudentController.java
@@ -1,18 +1,153 @@
 package com.academics.controllers;
 
-import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
+
+
 
 @RestController
 public class StudentController {
 	
-	@GetMapping("/getName")
-	public String getName() {
-		return "Anurag";
+	@RequestMapping("/form")
+	public String form() {
+		
+		return "form";
 	}
+	
+	@RequestMapping("/welcome")
+	public String welcome( ) {
+		return "welcome";
+	}
+	
+	
+	@RequestMapping("/photoUpload")
+	public String photoUpload() {
+		return "photoUpload";
+	}
+	@RequestMapping("/admissionForm")
+	public String admissionForm() {
+		
+		return "admissionForm";
+	}	
+	
+	@RequestMapping(value ="/stusignup", method = RequestMethod.GET)
+	public String stuSignup() {
 
-	@GetMapping("/getjsp")
-	public String getView() {
-		return "view-books";
+		return "stuSignup";
+	}
+	
+	@RequestMapping("/adLogin")
+	public String adLogin() {
+
+		return "adLogin";
+	}
+	
+	@RequestMapping("/submitFees")
+	public String submitFees() {
+		
+		return "submitFees";
+	}
+	
+	@RequestMapping("/feesSummary")
+	public String feesSummary() {
+			
+			return "feesSummary";
+	}
+	
+	
+	@RequestMapping("/feesReport")
+	public String feesReport() {
+			return "feesReport";
+	}
+	
+	@RequestMapping("/feesReportGenerate")
+	public String feesReportGenerate() {
+		
+		return "feesReport";
+	}	
+	
+	@RequestMapping("/feesProcessing")
+	public String feesProcessing() {
+			
+		
+			return "redirect:/feesSummary";
+	}
+	
+	
+	
+	@RequestMapping(value="/searchStudents")
+	public String searchByBranch() {	
+		
+		return "studentsList";
+	}
+	
+	@RequestMapping("/searchByRollNo")
+	public String searchByRollNo() {
+		
+		return "studentsList";
+	}
+	
+	@RequestMapping("/searchByClass")
+	public String searchByClass() {
+		
+		return "studentsList";
+	}
+	
+	@RequestMapping("/studentsList")
+	public String studentsList() {
+		
+		return "studentsList";
+	}
+	
+	
+	@RequestMapping("/addStudents")
+	public String addStudents() {
+		
+		return "registerStudents";
+	}
+	
+	@RequestMapping("/updateStudents")
+	public String updateStudent() {
+		
+		return "admissionForm";
+	}
+	
+	
+	
+	@RequestMapping("/deleteStudent")
+	public String deleteStudent() {
+		
+		return "redirect:/studentsList";
+	}
+	
+	
+
+	@RequestMapping("/addStudentsToDb")
+	public String addStudentsToDb() {
+		
+		return "redirect:/addStudents";
+	}
+	
+	
+	@RequestMapping(value="/saveAdmissison",method=RequestMethod.POST)
+	public String saveAdmissison()  {
+	
+		return "redirect:/studentsList";
+		
+	}
+	
+	@RequestMapping("/customlogin")
+	public String customLogin() {
+
+		return "customLogin";
+	}
+	
+	@ResponseBody
+	@RequestMapping("/accessdenied")
+	public String accessDenied() {
+		
+		return "Access Denied";
 	}
 }
