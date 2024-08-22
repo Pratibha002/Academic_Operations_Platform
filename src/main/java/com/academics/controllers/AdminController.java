230b4151fabd4593f180cb32b5f83b94b8f87cf8
AnuragShukla-2923 <Shukla.Anurag2923@gmail.com>
2024-08-22 15:31:52 +0530

    admin jsp pages with basic validations

--- src/main/java/com/academics/controllers/AdminController.java
+++ src/main/java/com/academics/controllers/AdminController.java
@@ -1,92 +1,88 @@
 package com.academics.controllers;
 
-import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.RequestParam;
 
-
-@RestController
+//@RestController
+@Controller
 public class AdminController {
-	@Autowired
-	//public AdminDao adminDao;
-	
-	
-	
-	
+//	@Autowired
+	// public AdminDao adminDao;
+
 	@RequestMapping("/classes")
 	public String classes() {
-		
+
 		return "addClasses";
 	}
-	
+
 	@RequestMapping("/addClasses")
-	public String addClasses() {
-	
-		return "redirect:/classes";
+	public String addClasses(@RequestParam("className") String className, @RequestParam("feesAmount") int feesAmount) {
+
+		System.out.println(className + "--" + feesAmount);
+		return "redirect:/academic/classes";
 	}
-	
-	
+
 	@RequestMapping("/session")
 	public String session() {
-		
+
 		return "addSession";
 	}
-	
+
 	@RequestMapping("/addsession")
-	public String addSession() {
-	
-		return "redirect:/session";
+	public String addSession(@RequestParam("sessionStart") String sessionStart,
+			@RequestParam("sessionEnd") String sessionEnd) {
+
+		System.out.println(sessionStart + "---" + sessionEnd);
+		return "redirect:/academic/session";
 	}
-	
 
 	@RequestMapping("/category")
 	public String category() {
-		
+
 		return "addCategory";
 	}
-	
+
 	@RequestMapping("/addcategory")
-	public String addCategory() {
-	
-		return "redirect:/category";
+	public String addCategory(@RequestParam("category") String category) {
+		System.out.println(category + "----");
+		return "redirect:/academic/category";
 	}
-	
 
 	@RequestMapping("/feestype")
 	public String feestype() {
-		
+
 		return "addFeesType";
 	}
-	
+
 	@RequestMapping("/addfeestype")
 	public String addfeestype() {
 
 		return "redirect:/feestype";
 	}
-	
+
 	@RequestMapping("/deletefeestype")
 	public String delteFeesType() {
-		
+
 		return "redirect:/feestype";
 	}
-	
+
 	@RequestMapping("/deleteclasses")
 	public String delteClasses() {
-		
+
 		return "redirect:/classes";
 	}
-	
-	
+
 	@RequestMapping("/deletesession")
 	public String delteSession() {
-		
+
 		return "redirect:/session";
 	}
+
 	@RequestMapping("/deletecategory")
 	public String delteCategory() {
-		
+
 		return "redirect:/category";
 	}
-	
 
 }
--- src/main/java/com/academics/controllers/RestC.java
+++ src/main/java/com/academics/controllers/RestC.java
@@ -3,15 +3,13 @@ package com.academics.controllers;
 import java.io.IOException;
 import java.util.Base64;
 
-import javax.servlet.annotation.MultipartConfig;
-
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.multipart.MultipartFile;
 
-@MultipartConfig
+//@MultipartConfig
 @Controller
 public class RestC {
 	
